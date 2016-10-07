287456e4e80ef5dca5007cee3dc485ef36d77f6a
Dhana <dkanesh@gmail.com>
2016-10-07 20:47:53 +0000

    Test for ip_address_list is failing.

--- ip_anon_test.py
+++ ip_anon_test.py
@@ -13,6 +13,7 @@ IOError: [Errno 2] No such file or directory: 'ample_conf.txt'
 
 class TestIPAnon(unittest.TestCase):
     test_ips = ['171.68.120.214', '40.31.1.144', '255.255.255.0', '10.14.14.14', '255.255.255.0', '40.31.1.50', '40.31.1.60', '255.255.255.0', '10.14.14.15', '40.31.1.77', '40.31.1.1', '171.68.118.0', '255.255.255.0', '40.31.1.1', '171.68.120.0', '255.255.255.0', '40.31.1.1', '171.68.118.143', '40.31.1.144', '171.68.118.143', '171.68.118.143', '171.68.118.143', '171.68.120.214']
+    test_interface_objects = [IPv4Interface('40.31.1.144/24'), IPv4Interface('10.14.14.14/24'), IPv4Interface('40.31.1.60/24'), IPv4Interface('171.68.118.0/24'), IPv4Interface('171.68.120.0/24')]
     def setUp(self):
         self.ip_anon = IPAnon('sample_conf.txt')
 
@@ -28,7 +29,10 @@ class TestIPAnon(unittest.TestCase):
         self.assertEqual(TestIPAnon.test_ips, t_ips)
 
     def test_ip_address_list(self):
-        self.assertTrue(True, t.address_list(ip))
+        t = IPAnon('sample_conf.txt')
+        t.ip_guess()
+        t.ip_address_list()
+        self.assertEqual(test_interface_objects, t.config_interfaces)
 
 if __name__ == '__main__':
     unittest.main()
